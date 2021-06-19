28f4501970fd43b2232451358e075bbf3701c042
yangyusok <diddbtjr7@naver.com>
2021-06-19 16:15:24 +0900

    프로젝트 생성

---
+++ src/main/java/com/flab/foodeats/FoodEatsApplication.java
@@ -0,0 +1,13 @@
+package com.flab.foodeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodEatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodEatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flab/foodeats/FoodEatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.foodeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodEatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
