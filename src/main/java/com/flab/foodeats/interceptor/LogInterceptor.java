a5d85cef93f2c9481544cf4170bc629a954bcf7b
EunSoo JIN <dudtn6788@naver.com>
2021-07-12 17:38:12 +0900

    필터 & 어노테이션

--- src/main/java/com/flab/foodeats/interceptor/LogInterceptor.java
+++ src/main/java/com/flab/foodeats/interceptor/LogInterceptor.java
@@ -21,25 +21,32 @@ public class LogInterceptor implements HandlerInterceptor {
         String uuid = UUID.randomUUID().toString();
         request.setAttribute(LOG_ID, uuid);
 
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+
         if (handler instanceof HandlerMethod) {
             HandlerMethod hm = (HandlerMethod) handler; //호출할 컨트롤러 메서드의모든 정보가 포함되어 있다.
         }
         log.info("REQUEST [{}][{}][{}]", uuid, requestURI, handler);
         return true; //false 진행X
+
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse
             response, Object handler, ModelAndView modelAndView) throws Exception {
+
         log.info("postHandle [{}]", modelAndView);  // modelAndView 사용 안하는 중! 사용해서 구현해볼 것
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse
             response, Object handler, Exception ex) throws Exception {
+
         String requestURI = request.getRequestURI();
         String logId = (String)request.getAttribute(LOG_ID);
         log.info("RESPONSE [{}][{}]", logId, requestURI);
+
         if (ex != null) {
             log.error("afterCompletion error!!", ex);
         }
