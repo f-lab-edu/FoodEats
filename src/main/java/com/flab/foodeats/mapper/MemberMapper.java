6f69c28fa7952ed51e844a749ae31a0caa4f7f31
EunSoo JIN <dudtn6788@naver.com>
2021-07-12 17:35:13 +0900

    Mapper 수정

--- src/main/java/com/flab/foodeats/mapper/MemberMapper.java
+++ src/main/java/com/flab/foodeats/mapper/MemberMapper.java
@@ -3,6 +3,9 @@ package com.flab.foodeats.mapper;
 import com.flab.foodeats.model.LoginForm;
 import com.flab.foodeats.model.Member;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * 로그인 기반 코드
@@ -11,15 +14,27 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MemberMapper {
-    // 회원정보 저장
-    void save(String arg0, String arg1, String arg2);
-    // 비밀번호 찾기
-    String findPassword(String arg0);
-    // 특정회원의 id, password만 받아오기
-    LoginForm findIdPass(String arg0);
-    // 특정회원의 정보 수정하기
-    int updateInfo(String arg0,String arg1, String arg2);
 
-    // 특정회원의 모든 정보 찾아오기 (추후 사용을 위해)
-    Member findAll(String arg0);
+    // 회원가입
+    void save(@Param("id") String id, @Param("password") String password, @Param("name") String name);
+
+    // 로그인  (=비밀번호 찾기)
+    String findPassword(String id);
+
+    // 회원정보 수정
+    int  updateInfo(@Param("id") String id, @Param("password") String password, @Param("name") String name);
+
+    // 회원 탈퇴
+    int deleteUserInfo(@Param("id") String id);
+
+    // 모든회원 조회
+    List<Member> findAll();
+
+    // 단일회원 조회
+    Member findMemberById(String id);
+
+
+
+
+
 }
