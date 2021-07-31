c27a750073709624821e831423f95891fa60c6c8
EunSoo JIN <dudtn6788@naver.com>
2021-07-31 11:02:46 +0900

    코드표 작성 (성공, 실패, 상태)

--- src/main/java/com/flab/foodeats/user/model/ResponseStatus.java
+++
@@ -1,7 +0,0 @@
-package com.flab.foodeats.user.model;
-
-public enum ResponseStatus {
-	SUCCESS,
-	FAIL,
-	ERROR
-}
---
+++ src/main/java/com/flab/foodeats/user/model/code/StatusUserCode.java
@@ -0,0 +1,10 @@
+package com.flab.foodeats.user.model.code;
+
+/**
+ * ResponseResult - ResponseStatus
+ */
+public enum StatusUserCode {
+	SUCCESS,
+	FAIL,
+	ERROR
+}
