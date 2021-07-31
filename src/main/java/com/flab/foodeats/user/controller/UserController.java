8c8ff3566a059f91f792ed67c475dfcb56d5bd81
EunSoo JIN <dudtn6788@naver.com>
2021-07-31 11:00:51 +0900

    Controller 코드 리팩토링

--- src/main/java/com/flab/foodeats/user/controller/UserController.java
+++ src/main/java/com/flab/foodeats/user/controller/UserController.java
@@ -4,41 +4,37 @@ import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import com.flab.foodeats.user.interceptor.UserLoginAuth;
 import com.flab.foodeats.user.model.DeleteFormDTO;
 import com.flab.foodeats.user.model.InsertFormDTO;
 import com.flab.foodeats.user.model.LoginFormDTO;
 import com.flab.foodeats.user.model.UpdateFormDTO;
 
-public interface MemberController {
+public interface UserController {
 
 	@PostMapping("/insert")
-	public ResponseEntity<?> insertUser(@Valid @RequestBody InsertFormDTO insertFormDTO, BindingResult bindingResult);
+	public ResponseEntity<?> insertUser(@Valid @RequestBody InsertFormDTO insertFormDTO);
 
 	@PostMapping("/login")
-	public ResponseEntity<?> login(@Valid @RequestBody LoginFormDTO loginFormDTO, BindingResult bindingResult,
-		HttpSession httpSession);
+	public ResponseEntity<?> login(@Valid @RequestBody LoginFormDTO loginFormDTO, HttpSession httpSession);
 
 	@PostMapping("/logout")
-	public ResponseEntity<Void> logout(HttpSession httpSession);
+	public ResponseEntity<?> logout(HttpSession httpSession);
 
 	@GetMapping("/all")
 	public ResponseEntity<?> findAll();
 
-	@UserLoginAuth
-	@PutMapping("/update")
-	public ResponseEntity<?> update(@Valid @RequestBody UpdateFormDTO updateFormDTO, BindingResult bindingResult,
-		HttpSession httpSession);
+	@GetMapping("/findInfo")
+	public ResponseEntity<?> findInfoById(@Valid @RequestBody LoginFormDTO loginFormDTO);
 
-	@UserLoginAuth
+	@PutMapping("/update")
+	public ResponseEntity<?> update(@Valid @RequestBody UpdateFormDTO updateFormDTO);
+
 	@DeleteMapping("/delete")
-	public ResponseEntity<?> deleteUser(@Valid @RequestBody DeleteFormDTO deleteFormDTO, BindingResult bindingResult,
-		HttpSession httpSession);
+	public ResponseEntity<?> deleteUser(@Valid @RequestBody DeleteFormDTO deleteFormDTO);
 }
