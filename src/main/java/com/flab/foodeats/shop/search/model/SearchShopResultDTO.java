a235548baff55b6204b39ea96d7da0ab6e743d63
yusok7 <diddbtjr7@naver.com>
2021-08-11 15:20:50 +0900

    검색 후 결과 binding용 dto추가

---
+++ src/main/java/com/flab/foodeats/shop/search/model/SearchShopResultDTO.java
@@ -0,0 +1,28 @@
+package com.flab.foodeats.shop.search.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class SearchShopResultDTO {
+
+	@JsonProperty
+	private int shopId;
+	@JsonProperty
+	private String category;
+	@JsonProperty
+	private String brand;
+	@JsonProperty
+	private String location;
+	@JsonProperty
+	private String phone;
+
+	public SearchShopResultDTO() {
+	}
+
+	public SearchShopResultDTO(int shopId, String category, String brand, String location, String phone) {
+		this.shopId = shopId;
+		this.category = category;
+		this.brand = brand;
+		this.location = location;
+		this.phone = phone;
+	}
+}
