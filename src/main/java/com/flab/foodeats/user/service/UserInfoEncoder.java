a976258d29103afb89390409ea7e82d5422a51fa
Maestro6788 <83272619+Maestro6788@users.noreply.github.com>
2021-08-11 19:08:00 +0900

    Update UserInfoEncoder.java
    UserInfoEncoder 클래스

--- src/main/java/com/flab/foodeats/user/service/UserInfoEncoder.java
+++ src/main/java/com/flab/foodeats/user/service/UserInfoEncoder.java
@@ -1,14 +1,31 @@
 package com.flab.foodeats.user.service;
 
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class UserInfoEncoder {
 
-	public String passwordEncoder(String inputPassword) {
+
+	private String password;
+
+
+	public UserInfoEncoder() {
+
+	}
+
+	public UserInfoEncoder(String password) {
+		this.password = password;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String encodePassword(String wantToEncodePasswordEncode) {
+		this.password = encoder(wantToEncodePasswordEncode);
+		return this.password;
+	}
+
+	public String encoder(String beforeEncodePassword) {
 		StringBuffer result = new StringBuffer();
 
 		try {
@@ -17,7 +34,7 @@ public class UserInfoEncoder {
 			byte[] salt = "Hello! This is Salt.".getBytes();
 			digest.reset();
 			digest.update(salt);
-			byte[] chars = digest.digest(inputPassword.getBytes("UTF-8"));
+			byte[] chars = digest.digest(beforeEncodePassword.getBytes("UTF-8"));
 
 			for(int i=0;i<chars.length;i++) {
 				String hex = Integer.toHexString(0xff & chars[i]);
