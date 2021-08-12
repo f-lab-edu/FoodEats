c96a17149bf70ef512de46b8d8322714c61a96c3
EunSoo JIN <dudtn6788@naver.com>
2021-08-12 11:07:00 +0900

    가맹점 로그인 SQL 쿼리문 추가

--- src/main/java/com/flab/foodeats/user/mapper/UserMapper.java
+++ src/main/java/com/flab/foodeats/user/mapper/UserMapper.java
@@ -13,16 +13,26 @@ public interface UserMapper {
 
 	// 회원가입 - 소비자
 	void registerConsumerUser(@Param("insertFormDTO") InsertFormDTO insertFormDTO);
+	// 회원가입 - 가맹점
+	void registerShopUser(@Param("insertFormDTO") InsertFormDTO insertFormDTO);
 
 	// 로그인 - 소비자
 	String findConsumerUserPassword(String id);
+	// 로그인 - 가맹점
+	String findShopUserPassword(String id);
 
 	// 회원 수정 - 소비자
 	void updateConsumerUserInfo(@Param("id") String id, @Param("updateFormDTO") UpdateFormDTO updateFormDTO);
+	// 회원 수정 - 가맹점
+	void updateShopUserInfo(@Param("id") String id, @Param("updateFormDTO") UpdateFormDTO updateFormDTO);
 
 	// 회원 탈퇴 - 소비자
 	void deleteConsumerUserInfo(String id);
+	// 회원 탈퇴 - 가맹점
+	void deleteShopUserInfo(String id);
 
 	// 단일 회원 조회 - 소비자
 	InsertFormDTO findConsumerUserById(String id);
+	// 단일 회원 조회 - 가맹점
+	InsertFormDTO findShopUserById(String id);
 }
