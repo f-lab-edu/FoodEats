5f8b53c3a7f7ff756db70705c8671f4262797674
Maestro6788 <83272619+Maestro6788@users.noreply.github.com>
2021-08-13 11:31:39 +0900

    피드백 반영 / 인터셉터 요청 url 검증 로직 수정

--- src/main/java/com/flab/foodeats/user/interceptor/AuthInterceptor.java
+++ src/main/java/com/flab/foodeats/user/interceptor/AuthInterceptor.java
@@ -6,6 +6,8 @@ import com.flab.foodeats.user.interceptor.auth.AuthErrorCheck;
 import com.flab.foodeats.user.interceptor.auth.AuthSessionControl;
 import com.flab.foodeats.user.interceptor.auth.AuthRequired;
 import com.flab.foodeats.global.ApiResponse;
+import com.flab.foodeats.user.interceptor.auth.ShopAuthSessionControl;
+import com.flab.foodeats.user.interceptor.auth.ShopAuth;
 import com.flab.foodeats.user.model.code.ErrorUserCode;
 import com.flab.foodeats.global.StatusCode;
 
@@ -40,24 +42,20 @@ public class AuthInterceptor implements HandlerInterceptor {
 
 		//Session
 		HttpSession session = request.getSession(false);
+		String RequestURI = String.valueOf(request.getRequestURI());
 
 		try {
-			String auth = "";
-			if(request.getRequestURI().equals("/user/consumer/update") || request.getRequestURI().equals("/user/consumer/delete")){
-				auth = (String)session.getAttribute(Auth.CUNSUMER_KEY);
+			if (RequestURI.startsWith("/user/consumer/")) {
+				String auth = (String)session.getAttribute(Auth.CUNSUMER_KEY);
+				AuthSessionControl.setAuthentication(auth);
 			}
-			if(request.getRequestURI().equals("/user/shop/update") || request.getRequestURI().equals("/user/shop/delete")){
-				auth = (String)session.getAttribute(Auth.SHOP_KEY);
+
+			if (RequestURI.startsWith("/user/shop/")) {
+				ShopAuth shopAuth = (ShopAuth)session.getAttribute(ShopAuth.SHOP_KEY);
+				AuthSessionControl.setShopAuthentication(shopAuth);
 			}
-			AuthSessionControl.setAuthentication(auth);
 		} catch (Exception e) {
 			throw new Exception(ErrorUserCode.SESSION_NO_AUTHORIZED.getMessage());
-		}
-
-		if (!authErrorCheck.sessionNullCheck(session)) {
-			ApiResponse msg = new ApiResponse(StatusCode.FAIL, ErrorUserCode.SESSION_NO_AUTHORIZED.getMessage());
-			response.getWriter().write(mapper.writeValueAsString(msg));
-			return false;
 		}
 
 		if (!authErrorCheck.authAnnotationNullCheck(filter)) {
