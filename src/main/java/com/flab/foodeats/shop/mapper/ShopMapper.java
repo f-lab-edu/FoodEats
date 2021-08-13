5b202169e6ce7e279fff8549697879c4f19b46a4
EunSoo JIN <dudtn6788@naver.com>
2021-08-13 20:05:45 +0900

    가맹점 상태 정보 Mapper / SQL.XML

--- src/main/java/com/flab/foodeats/shop/mapper/ShopMapper.java
+++ src/main/java/com/flab/foodeats/shop/mapper/ShopMapper.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.flab.foodeats.shop.model.EssentialShopInfo;
+import com.flab.foodeats.shop.model.StatusShopInfo;
 
 @Mapper
 @Repository
@@ -20,8 +21,18 @@ public interface ShopMapper {
 	// 가맹점 삭제 (필수)
 	void deleteEssentialInfo(Integer shopId);
 
+	// 가맹점 등록 (상태)
+	void registerStatusInfo(@Param("statusShopInfo") StatusShopInfo statusShopInfo, Integer shopId);
+
+	// 가맹점 수정 (상태)
+	void updateStatusInfo(@Param("statusShopInfo") StatusShopInfo statusShopInfo, Integer shopId);
+
 	// 가맹점 기본정보 조회 By ShopId
 	Integer findEssentialInfoById(Integer shopId);
+
+	// 가맹점 상태정보 조회 By ShopId/
+	Integer findStatusInfoById(Integer shopId);
+
 
 	// 가맹점 기본정보 전체 조회
 	List<EssentialShopInfo> ShopListAllInfo();
