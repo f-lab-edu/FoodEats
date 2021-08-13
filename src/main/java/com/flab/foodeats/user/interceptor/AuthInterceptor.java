2c9412d47844afbfed77182d1699e3c85e0af408
Maestro6788 <83272619+Maestro6788@users.noreply.github.com>
2021-08-13 11:43:05 +0900

    [#46] 가맹점 로그인 추가 (#50)
    * 가맹점 로그인 controller 추가
    
    * 가맹점 로그인 Service 추가
    
    * 로그인 인증시 소비자/가맹점 구별
    
    * 가맹점 로그인 SQL 쿼리문 추가
    
    * 피드백 반영 / 인터셉터 요청 url 검증 로직 수정
    
    Co-authored-by: EunSoo JIN <dudtn6788@naver.com>

--- src/main/java/com/flab/foodeats/user/interceptor/AuthInterceptor.java
+++ src/main/java/com/flab/foodeats/user/interceptor/AuthInterceptor.java
@@ -6,6 +6,8 @@ import com.flab.foodeats.user.interceptor.auth.AuthErrorCheck;
 import com.flab.foodeats.user.interceptor.auth.AuthSessionControl;
 import com.flab.foodeats.user.interceptor.auth.AuthRequired;
 import com.flab.foodeats.global.ApiResponse;
+import com.flab.foodeats.user.interceptor.auth.ShopAuthSessionControl;
+import com.flab.foodeats.user.interceptor.auth.ShopAuth;
 import com.flab.foodeats.user.model.code.ErrorUserCode;
 import com.flab.foodeats.global.StatusCode;
 
@@ -40,17 +42,20 @@ public class AuthInterceptor implements HandlerInterceptor {
 
 		//Session
 		HttpSession session = request.getSession(false);
+		String RequestURI = String.valueOf(request.getRequestURI());
+
 		try {
-			String auth = (String)session.getAttribute(Auth.CUNSUMER_KEY);
-			AuthSessionControl.setAuthentication(auth);
+			if (RequestURI.startsWith("/user/consumer/")) {
+				String auth = (String)session.getAttribute(Auth.CUNSUMER_KEY);
+				AuthSessionControl.setAuthentication(auth);
+			}
+
+			if (RequestURI.startsWith("/user/shop/")) {
+				ShopAuth shopAuth = (ShopAuth)session.getAttribute(ShopAuth.SHOP_KEY);
+				AuthSessionControl.setShopAuthentication(shopAuth);
+			}
 		} catch (Exception e) {
 			throw new Exception(ErrorUserCode.SESSION_NO_AUTHORIZED.getMessage());
-		}
-
-		if (!authErrorCheck.sessionNullCheck(session)) {
-			ApiResponse msg = new ApiResponse(StatusCode.FAIL, ErrorUserCode.SESSION_NO_AUTHORIZED.getMessage());
-			response.getWriter().write(mapper.writeValueAsString(msg));
-			return false;
 		}
 
 		if (!authErrorCheck.authAnnotationNullCheck(filter)) {
--- src/main/java/com/flab/foodeats/user/user/shop/ShopUserServiceImpl.java
+++ src/main/java/com/flab/foodeats/user/user/shop/ShopUserServiceImpl.java
@@ -1,12 +1,12 @@
-package com.flab.foodeats.user.user.consumer;
+package com.flab.foodeats.user.user.shop;
 
 import org.springframework.stereotype.Service;
 
+import com.flab.foodeats.global.ApiResponse;
+import com.flab.foodeats.global.StatusCode;
 import com.flab.foodeats.user.mapper.UserMapper;
 import com.flab.foodeats.user.model.InsertFormDTO;
 import com.flab.foodeats.user.model.LoginFormDTO;
-import com.flab.foodeats.global.ApiResponse;
-import com.flab.foodeats.global.StatusCode;
 import com.flab.foodeats.user.model.UpdateFormDTO;
 import com.flab.foodeats.user.model.code.SuccessUserCode;
 import com.flab.foodeats.user.service.InsertErrorCheck;
@@ -14,57 +14,59 @@ import com.flab.foodeats.user.service.LoginErrorCheck;
 import com.flab.foodeats.user.service.UserInfoEncoder;
 
 @Service
-public class ConsumerUserServiceImpl implements ConsumerUserService {
+public class ShopUserServiceImpl implements ShopUserService {
 
 	private UserMapper userMapper;
 	private InsertErrorCheck insertErrorCheck;
 	private LoginErrorCheck loginErrorCheck;
 
-	public ConsumerUserServiceImpl(UserMapper userMapper, InsertErrorCheck insertErrorCheck,
+	public ShopUserServiceImpl(UserMapper userMapper, InsertErrorCheck insertErrorCheck,
 		LoginErrorCheck loginErrorCheck) {
 		this.userMapper = userMapper;
 		this.insertErrorCheck = insertErrorCheck;
 		this.loginErrorCheck = loginErrorCheck;
 	}
 
-	// 소비자 - 회원가입
-	public ApiResponse registerConsumerUser(InsertFormDTO insertFormDTO) {
-		insertErrorCheck.idAlreadyExistCheck(userMapper.findConsumerUserById(insertFormDTO.getId()));
+	// 가맹점 - 회원가입
+	public ApiResponse registerShopUser(InsertFormDTO insertFormDTO) {
+		insertErrorCheck.idAlreadyExistCheck(userMapper.findShopUserById(insertFormDTO.getId()));
 		insertFormDTO.setPassword(new UserInfoEncoder().encodePassword(insertFormDTO.getPassword()));
-		userMapper.registerConsumerUser(insertFormDTO);
+		userMapper.registerShopUser(insertFormDTO);
 		ApiResponse apiResponse = new ApiResponse(StatusCode.SUCCESS, SuccessUserCode.USER_INSERT_SUCCESS);
 		return apiResponse;
 	}
 
-	// 소비자 - 로그인
+
+	// 가맹점 - 로그인
 	@Override
-	public ApiResponse loginConsumerUser(LoginFormDTO loginFormDTO) {
-		loginErrorCheck.notExistUserValid(userMapper.findConsumerUserPassword(loginFormDTO.getId()));
-		loginErrorCheck.validationLogin(userMapper.findConsumerUserPassword(loginFormDTO.getId()), loginFormDTO.getPassword());
+	public ApiResponse loginShopUser(LoginFormDTO loginFormDTO) {
+		loginErrorCheck.notExistUserValid(userMapper.findShopUserPassword(loginFormDTO.getId()));
+		loginErrorCheck.validationLogin(userMapper.findShopUserPassword(loginFormDTO.getId()), loginFormDTO.getPassword());
 		ApiResponse apiResponse = new ApiResponse(StatusCode.SUCCESS, SuccessUserCode.USER_LOGIN_SUCCESS);
 		return apiResponse;
 	}
 
-	// 소비자 - 로그아웃
+	// 가맹점 - 로그아웃
 	@Override
-	public ApiResponse logoutConsumerUser() {
+	public ApiResponse logoutShopUser() {
 		ApiResponse apiResponse = new ApiResponse(StatusCode.SUCCESS, SuccessUserCode.USER_LOGOUT_SUCCESS);
 		return apiResponse;
 	}
 
-	// 소비자 - 회원 수정
+
+	// 가맹점 - 회원 수정
 	@Override
-	public ApiResponse updateConsumerUser(UpdateFormDTO updateFormDTO, String authenticationInfo) {
+	public ApiResponse updateShopUser(UpdateFormDTO updateFormDTO, String authenticationInfo) {
 		updateFormDTO.setPassword(new UserInfoEncoder().encodePassword(updateFormDTO.getPassword()));
-		userMapper.updateConsumerUserInfo(authenticationInfo, updateFormDTO);
+		userMapper.updateShopUserInfo(authenticationInfo, updateFormDTO);
 		ApiResponse apiResponse = new ApiResponse(StatusCode.SUCCESS, SuccessUserCode.USER_UPDATE_SUCCESS);
 		return apiResponse;
 	}
 
-	// 소비자 - 회원 삭제
+	// 가맹점 - 회원 삭제
 	@Override
-	public ApiResponse deleteConsumerUser(String authenticationInfo) {
-		userMapper.deleteConsumerUserInfo(authenticationInfo);
+	public ApiResponse deleteShopUser(String authenticationInfo) {
+		userMapper.deleteShopUserInfo(authenticationInfo);
 		ApiResponse apiResponse = new ApiResponse(StatusCode.SUCCESS, SuccessUserCode.USER_DELETE_SUCCESS);
 		return apiResponse;
 	}
