60f9462bd2a062d349041e6818620a073e67e601
yusok7 <diddbtjr7@naver.com>
2021-08-15 21:36:15 +0900

    메뉴, 메뉴 옵션 수정 및 삭제결과를 위한 code 작성

--- src/main/java/com/flab/foodeats/menu/model/code/SuccessMenuCode.java
+++ src/main/java/com/flab/foodeats/menu/model/code/SuccessMenuCode.java
@@ -2,7 +2,11 @@ package com.flab.foodeats.menu.model.code;
 
 public enum SuccessMenuCode {
 
-	MENU_REGISTER_SUCCESS("Menu Register Success");
+	MENU_REGISTER_SUCCESS("Menu Register Success"),
+	MENU_UPDATE_SUCCESS("Menu Update Success"),
+	MENU_DELETE_SUCCESS("Menu DELETE Success"),
+	OPTION_UPDATE_SUCCESS("Option Update Success"),
+	OPTION_DELETE_SUCCESS("Option Delete Success");
 
 	private final String message;
 
