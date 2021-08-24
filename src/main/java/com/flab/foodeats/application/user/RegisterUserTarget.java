d9a0b15acaaebc835f68ed05e33915c34e787d4d
Maestro6788 <ensu7928@gmail.com>
2021-08-24 17:04:17 +0900

    Target 컬럼 추가 및 엔티티 전달

--- src/main/java/com/flab/foodeats/application/user/RegisterUserTarget.java
+++ src/main/java/com/flab/foodeats/application/user/RegisterUserTarget.java
@@ -1,17 +1,25 @@
 package com.flab.foodeats.application.user;
 
 import com.flab.foodeats.common.Encryption;
+import com.flab.foodeats.domain.user.User;
 
 public class RegisterUserTarget {
 
 	private final String userId;
 	private final String password;
 	private final String name;
+	private final String email;
+	private final String phone;
+	private final String address;
 
-	public RegisterUserTarget(String userId, String password, String name) {
+	public RegisterUserTarget(String userId, String password, String name, String email, String phone,
+		String address) {
 		this.userId = userId;
-		this.password = Encryption.encoder(password);
+		this.password = password;
 		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.address = address;
 	}
 
 	public String getUserId() {
@@ -24,5 +32,21 @@ public class RegisterUserTarget {
 
 	public String getName() {
 		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public User toEntity() {
+		return new User(userId, password, name, email, phone, address);
 	}
 }
