fad83f567e12fd44ec6936fbe6f14556111282af
Maestro6788 <ensu7928@gmail.com>
2021-08-25 17:27:37 +0900

    Shop 상태정보 등록 Entity

---
+++ src/main/java/com/flab/foodeats/domain/shop/Status.java
@@ -0,0 +1,60 @@
+package com.flab.foodeats.domain.shop;
+
+import java.time.LocalTime;
+
+public class Status {
+	private String shopId;
+	private LocalTime openTime;
+	private LocalTime closeTime;
+	private String closingDay;
+	private String shopOpenStatus;
+
+	public Status(String shopId, LocalTime openTime, LocalTime closeTime, String closingDay,
+		String shopOpenStatus) {
+		this.shopId = shopId;
+		this.openTime = openTime;
+		this.closeTime = closeTime;
+		this.closingDay = closingDay;
+		this.shopOpenStatus = shopOpenStatus;
+	}
+
+	public String getShopId() {
+		return shopId;
+	}
+
+	public void setShopId(String shopId) {
+		this.shopId = shopId;
+	}
+
+	public LocalTime getOpenTime() {
+		return openTime;
+	}
+
+	public void setOpenTime(LocalTime openTime) {
+		this.openTime = openTime;
+	}
+
+	public LocalTime getCloseTime() {
+		return closeTime;
+	}
+
+	public void setCloseTime(LocalTime closeTime) {
+		this.closeTime = closeTime;
+	}
+
+	public String getClosingDay() {
+		return closingDay;
+	}
+
+	public void setClosingDay(String closingDay) {
+		this.closingDay = closingDay;
+	}
+
+	public String getShopOpenStatus() {
+		return shopOpenStatus;
+	}
+
+	public void setShopOpenStatus(String shopOpenStatus) {
+		this.shopOpenStatus = shopOpenStatus;
+	}
+}
