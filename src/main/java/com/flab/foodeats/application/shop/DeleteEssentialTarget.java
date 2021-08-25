6fd7a4b1f56dc1406a6f7fac670089a22b128f15
Maestro6788 <ensu7928@gmail.com>
2021-08-25 16:37:46 +0900

    Shop 필수정보 삭제 dto

---
+++ src/main/java/com/flab/foodeats/application/shop/DeleteEssentialTarget.java
@@ -0,0 +1,18 @@
+package com.flab.foodeats.application.shop;
+
+public class DeleteEssentialTarget {
+
+	private String userId;
+
+	public DeleteEssentialTarget(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+}
