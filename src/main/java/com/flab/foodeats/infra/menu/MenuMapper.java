7f6caca79a00a084ec97894b490ed67c03843afe
yusok7 <diddbtjr7@naver.com>
2021-08-26 17:04:26 +0900

    Menu, MenuOption mapper 파일 작성

---
+++ src/main/java/com/flab/foodeats/infra/menu/MenuMapper.java
@@ -0,0 +1,21 @@
+package com.flab.foodeats.infra.menu;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.flab.foodeats.domain.menu.Menu;
+
+@Mapper
+public interface MenuMapper {
+
+	void registerMenu(@Param("shopId") int shopId, @Param("menu") Menu menu);
+
+	List<Menu> searchMenu(int shopId);
+
+	void modifyMenu(@Param("shopId") int shopId, @Param("menu")Menu menu);
+
+	void deleteMenu(int menuId);
+}
+
