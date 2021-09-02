6b3bca166c6fa9c7846ebe22b5a8ee667cdb59e0
Maestro6788 <ensu7928@gmail.com>
2021-09-02 14:55:05 +0900

    URL 변경 API /  Owner 권한 검증 SERVICE

--- src/main/java/com/flab/foodeats/application/shop/port/ShopService.java
+++ src/main/java/com/flab/foodeats/application/shop/port/ShopService.java
@@ -8,25 +8,26 @@ import com.flab.foodeats.application.shop.DeleteEssentialTarget;
 import com.flab.foodeats.application.shop.ConvenientTarget;
 import com.flab.foodeats.application.shop.EssentialTarget;
 import com.flab.foodeats.application.shop.StatusTarget;
+import com.flab.foodeats.common.auth.AuthInfo;
 import com.flab.foodeats.domain.shop.Essential;
 
 @Service
 public interface ShopService {
 
+
 	// 기본정보
-	void registerEssentialShopInfo(EssentialTarget target);
-	void updateEssentialShopInfo(EssentialTarget target);
-	void deleteEssentialShopInfo(DeleteEssentialTarget target);
+	void registerEssentialShopInfo(AuthInfo authInfo, EssentialTarget target);
+	void updateEssentialShopInfo(AuthInfo authInfo, EssentialTarget target);
+	void deleteEssentialShopInfo(AuthInfo authInfo, DeleteEssentialTarget target);
 
 	// 상태정보
-	void registerStatusShopInfo(StatusTarget target, String userId);
-	void updateStatusShopInfo(StatusTarget target, String userId);
+	void registerStatusShopInfo(AuthInfo authInfo, StatusTarget target);
+	void updateStatusShopInfo(AuthInfo authInfo, StatusTarget target);
 
 	// 편리정보
-	void registerConvenienceShopInfo(ConvenientTarget target, String userId);
-	void updateConvenienceShopInfo(ConvenientTarget target, String userId);
+	void registerConvenienceShopInfo(AuthInfo authInfo, ConvenientTarget target);
+	void updateConvenienceShopInfo(AuthInfo authInfo, ConvenientTarget target);
 
 	// 전체조회
-	List<Essential> searchShopAllInfo(StatusTarget target, String userId);
-
+	List<Essential> searchShopAllInfo(StatusTarget target);
 }
