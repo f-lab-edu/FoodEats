dfb8a4cb0aa358fd7e325aac1f83203cea443155
yusok <53485840+yusok7@users.noreply.github.com>
2021-09-13 10:49:10 +0900

    @EnableRedisHttpSession 삭제 및 RedisTemplate 사용을 위한 코드 추가

--- src/main/java/com/flab/foodeats/common/config/RedisSessionConfig.java
+++ src/main/java/com/flab/foodeats/common/config/RedisSessionConfig.java
@@ -6,9 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@EnableRedisHttpSession
+import com.example.redis3.common.AuthInfo;
+
 @Configuration
 public class RedisSessionConfig {
 
@@ -22,5 +25,14 @@ public class RedisSessionConfig {
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
     }
+    
+    @Bean
+	public RedisTemplate<String, AuthInfo> redisTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<String, AuthInfo> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+		redisTemplate.setConnectionFactory(connectionFactory);
+		return redisTemplate;
+	}
 
 }
