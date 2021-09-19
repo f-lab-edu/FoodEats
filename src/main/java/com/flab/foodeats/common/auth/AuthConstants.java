d3dfe6d1a2fb70734ed867dd29bdeb3d49327026
yusok7 <diddbtjr7@naver.com>
2021-09-19 23:08:24 +0900

    create AuthConstants.java

---
+++ src/main/java/com/flab/foodeats/common/auth/AuthConstants.java
@@ -0,0 +1,7 @@
+package com.flab.foodeats.common.auth;
+
+public class AuthConstants {
+
+	public static final String AUTH_HEADER = "Authorization";
+	public static final String TOKEN_TYPE = "BEARER";
+}
