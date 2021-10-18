ce9c643e29a64ca8d1675ea00a39b0e4ac2d5ea4
Maestro6788 <ensu7928@gmail.com>
2021-10-18 12:26:22 +0900

    refactor: shop JPA 적용 / Entity

--- src/main/java/com/flab/foodeats/domain/shop/BusinessHour.java
+++ src/main/java/com/flab/foodeats/domain/shop/BusinessHour.java
@@ -2,66 +2,79 @@ package com.flab.foodeats.domain.shop;
 
 import java.time.LocalTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@Table(name = "shop_business")
+@Entity
+@ToString
 public class BusinessHour {
+
+	@Id
+	@Column(name = "shop_business_no")
+	//@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long shopId;
+
+	@Column(name = "business_open_time", nullable = false)
 	private LocalTime openTime;
+
+	@Column(name = "business_close_time", nullable = false)
 	private LocalTime closeTime;
+
+	@Column(name = "business_closing_day", nullable = false)
 	private String closingDay;
+
+	@Column(name = "business_open_status", nullable = false)
 	private String shopOpenStatus;
+
+	// @OneToOne(mappedBy = "businessHour")
+	// private Shop shop;
+
+	// @OneToOne(fetch = FetchType.LAZY)
+	// @JoinColumn(name = "shop")
+	// private Shop shop;
 
 	public BusinessHour() {
 
 	}
 
-	public BusinessHour(Long shopId, LocalTime openTime, LocalTime closeTime, String closingDay, String shopOpenStatus) {
+	@Builder
+	public BusinessHour(Long shopId, LocalTime openTime, LocalTime closeTime, String closingDay,
+		String shopOpenStatus) {
 		this.shopId = shopId;
 		this.openTime = openTime;
 		this.closeTime = closeTime;
 		this.closingDay = closingDay;
 		this.shopOpenStatus = shopOpenStatus;
-	}
 
-	public BusinessHour(Long shopId) {
-		this.shopId = shopId;
-	}
-
-	public Long getShopId() {
-		return shopId;
-	}
-
-	public void setShopId(Long shopId) {
-		this.shopId = shopId;
-	}
-
-	public LocalTime getOpenTime() {
-		return openTime;
 	}
 
 	public void setOpenTime(LocalTime openTime) {
 		this.openTime = openTime;
 	}
 
-	public LocalTime getCloseTime() {
-		return closeTime;
-	}
-
 	public void setCloseTime(LocalTime closeTime) {
 		this.closeTime = closeTime;
-	}
-
-	public String getClosingDay() {
-		return closingDay;
 	}
 
 	public void setClosingDay(String closingDay) {
 		this.closingDay = closingDay;
 	}
 
-	public String getShopOpenStatus() {
-		return shopOpenStatus;
-	}
-
 	public void setShopOpenStatus(String shopOpenStatus) {
 		this.shopOpenStatus = shopOpenStatus;
 	}
+
 }
--- src/main/java/com/flab/foodeats/domain/shop/Shop.java
+++ src/main/java/com/flab/foodeats/domain/shop/Shop.java
@@ -1,63 +1,104 @@
 package com.flab.foodeats.domain.shop;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import com.flab.foodeats.domain.menu.Menu;
+import com.flab.foodeats.domain.user.Merchant;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@Table(name = "shop")
+@Entity
+@ToString
 public class Shop {
 
+	@Id
+	@Column(name = "shop_no")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long shopId;
+
+	@Column(name = "shop_category", nullable = false)
 	private String category;
+
+	@Column(name = "shop_brand", nullable = false)
 	private String brand;
+
+	@Column(name = "shop_location", nullable = false)
 	private String location;
+
+	@Column(name = "shop_phone", nullable = false)
 	private String phone;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "merchant")
+	private Merchant merchant;
+
+
+	@OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+	@JoinColumn(name = "businessHour")
+	private BusinessHour businessHour;
+
+	@OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+	@JoinColumn(name = "shopDelivery")
+	private ShopDelivery shopDelivery;
+
+	@OneToMany(mappedBy = "shop")
+	private List<Menu> menuList = new ArrayList<>();
+
+	// @OneToOne(mappedBy = "businessHour")
+	// private BusinessHour businessHour;
+	//
+	// @OneToOne(mappedBy = "shopDelivery")
+	// private ShopDelivery shopDelivery;
 
 	public Shop() {
 	}
 
-	public Shop(Long shopId, String category, String brand, String location, String phone) {
+	@Builder
+	public Shop(Long shopId, String category, String brand, String location, String phone,
+		Merchant merchant, BusinessHour businessHour, ShopDelivery shopDelivery,
+		List<Menu> menuList) {
 		this.shopId = shopId;
 		this.category = category;
 		this.brand = brand;
 		this.location = location;
 		this.phone = phone;
+		this.merchant = merchant;
+		this.businessHour = businessHour;
+		this.shopDelivery = shopDelivery;
+		this.menuList = menuList;
 	}
 
-	public Long getShopId() {
-		return shopId;
+	public void setMerchant(Merchant merchant) {
+		this.merchant = merchant;
 	}
 
-	public void setShopId(Long shopId) {
-		this.shopId = shopId;
+	public void setBusinessHour(BusinessHour businessHour) {
+		this.businessHour = businessHour;
 	}
 
-	public String getCategory() {
-		return category;
+	public void setShopDelivery(ShopDelivery shopDelivery) {
+		this.shopDelivery = shopDelivery;
 	}
 
-	public void setCategory(String category) {
-		this.category = category;
+	public void setMenuList(List<Menu> menuList) {
+		this.menuList = menuList;
 	}
-
-	public String getBrand() {
-		return brand;
-	}
-
-	public void setBrand(String brand) {
-		this.brand = brand;
-	}
-
-	public String getLocation() {
-		return location;
-	}
-
-	public void setLocation(String location) {
-		this.location = location;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-
 }
