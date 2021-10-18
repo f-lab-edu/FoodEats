52aaa59e0aee5c7fe99e2c035b5b62f53fab9df8
Maestro6788 <ensu7928@gmail.com>
2021-10-18 12:30:17 +0900

    refactor: user 간단한 코드 수정

--- src/main/java/com/flab/foodeats/domain/user/Merchant.java
+++ src/main/java/com/flab/foodeats/domain/user/Merchant.java
@@ -1,13 +1,18 @@
 package com.flab.foodeats.domain.user;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.flab.foodeats.common.Encryption;
+import com.flab.foodeats.domain.shop.Shop;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -42,6 +47,9 @@ public class Merchant {
 	@Column(name = "merchant_address", nullable = false)
 	private String address;
 
+	// @OneToMany(mappedBy = "merchant")
+	// private List<Shop> shopInfo = new ArrayList<>();
+
 	public Merchant() {
 	}
 
@@ -60,6 +68,4 @@ public class Merchant {
 		this.phone = phone;
 		this.address = address;
 	}
-
-
 }
