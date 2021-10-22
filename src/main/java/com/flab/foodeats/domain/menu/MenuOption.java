14ffcd232af92d67c636994caaf0f34969b4719c
Maestro6788 <ensu7928@gmail.com>
2021-10-22 17:33:29 +0900

    refactor: MenuOption JPA 적용 / Entity 단방향 매핑 (MENU <> MENUOPTION)

--- src/main/java/com/flab/foodeats/domain/menu/MenuOption.java
+++ src/main/java/com/flab/foodeats/domain/menu/MenuOption.java
@@ -1,47 +1,67 @@
 package com.flab.foodeats.domain.menu;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+import com.flab.foodeats.domain.user.Merchant;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@Table(name = "menu_option")
+@Entity
+@ToString
 public class MenuOption {
 
-	private long menuId;
-	private long menuOptionId;
+	@Id
+	@Column(name = "menu_option_no")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long menuOptionNo;
+
+	@Column(name = "menu_id", nullable = false)
+	private Long menuId;
+
+	@Column(name = "menu_option_name", nullable = false)
 	private String menuOptionName;
-	private int menuOptionPrice;
+
+	@Column(name = "menu_option_price", nullable = false)
+	private Integer menuOptionPrice;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "menu")
+	private Menu menu;
 
 	public MenuOption() {
 	}
 
-	public MenuOption(long menuId, String menuOptionName, int menuOptionPrice) {
+	@Builder
+	public MenuOption(Long menuOptionNo, long menuId, String menuOptionName, int menuOptionPrice,
+		Menu menu) {
+		this.menuOptionNo = menuOptionNo;
 		this.menuId = menuId;
 		this.menuOptionName = menuOptionName;
 		this.menuOptionPrice = menuOptionPrice;
+		this.menu = menu;
 	}
 
-	public MenuOption(long menuId, long menuOptionId, String menuOptionName, int menuOptionPrice) {
-		this.menuId = menuId;
-		this.menuOptionId = menuOptionId;
+	public void setMenuOptionName(String menuOptionName) {
 		this.menuOptionName = menuOptionName;
+	}
+
+	public void setMenuOptionPrice(Integer menuOptionPrice) {
 		this.menuOptionPrice = menuOptionPrice;
 	}
 
-	public void updateOption(String menuOptionName, int menuOptionPrice) {
-		this.menuOptionName = menuOptionName;
-		this.menuOptionPrice = menuOptionPrice;
-	}
-
-	public long getMenuId() {
-		return menuId;
-	}
-
-	public long getMenuOptionId() {
-		return menuOptionId;
-	}
-
-	public String getMenuOptionName() {
-		return menuOptionName;
-	}
-
-	public int getMenuOptionPrice() {
-		return menuOptionPrice;
+	public void setMenu(Menu menu) {
+		this.menu = menu;
 	}
 }
