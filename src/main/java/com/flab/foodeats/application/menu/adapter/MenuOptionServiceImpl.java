347d2452ea0757488b066a1a4131850c749befc7
Maestro6788 <ensu7928@gmail.com>
2021-10-22 17:33:59 +0900

    refactor: MenuOption JPA 적용 / Service

--- src/main/java/com/flab/foodeats/application/menu/adapter/MenuOptionServiceImpl.java
+++ src/main/java/com/flab/foodeats/application/menu/adapter/MenuOptionServiceImpl.java
@@ -1,5 +1,6 @@
 package com.flab.foodeats.application.menu.adapter;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,67 +12,96 @@ import com.flab.foodeats.application.menu.OptionTarget;
 import com.flab.foodeats.application.menu.port.MenuOptionService;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.flab.foodeats.common.auth.AuthInfo;
 import com.flab.foodeats.common.error.exception.UnauthorizedException;
+import com.flab.foodeats.common.response.ErrorUserCode;
+import com.flab.foodeats.domain.menu.Menu;
 import com.flab.foodeats.domain.menu.MenuOption;
-import com.flab.foodeats.infra.menu.MenuOptionMapper;
-import com.flab.foodeats.infra.shop.ShopMapper;
-import com.flab.foodeats.infra.user.UserMapper;
+import com.flab.foodeats.domain.shop.Shop;
+import com.flab.foodeats.infra.menu.MenuOptionRepository;
+import com.flab.foodeats.infra.menu.MenuRepository;
+import com.flab.foodeats.infra.shop.ShopRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class MenuOptionServiceImpl implements MenuOptionService {
 
-	private final MenuOptionMapper menuOptionMapper;
-	private final UserMapper userMapper;
+	private final MenuOptionRepository menuOptionRepository;
+	private final MenuRepository menuRepository;
+	private final ShopRepository shopRepository;
 
-	public MenuOptionServiceImpl(MenuOptionMapper menuOptionMapper, UserMapper userMapper) {
-		this.menuOptionMapper = menuOptionMapper;
-		this.userMapper = userMapper;
+	@Override
+	public void registerMenuOption(long shopId, long menuId, OptionRequest optionRequests, AuthInfo authInfo) {
+		// 권한검사
+		Shop shop = shopRepository.findById(shopId)
+			.orElseThrow(() -> new NullPointerException(ErrorUserCode.SHOP_INFO_NOT_EXIST.getMessage()));
+		checkIsAuthorizedUser(shop.getMerchant().getId(), authInfo.getId());
+
+		List<MenuOption> menuOptions = menuOptionRepository.findAllByMenuId(shopId);
+		checkIsExistOptionMenu(menuOptions, optionRequests);
+
+		// menu 객체 매핑
+		Menu menu = menuRepository.findById(shopId)
+			.orElseThrow(() -> new NullPointerException(ErrorUserCode.SHOP_INFO_NOT_EXIST.getMessage()));
+		MenuOption menuOption = optionRequests.toEntity(menuId);
+		menuOption.setMenu(menu);
+
+		menuOptionRepository.save(menuOption);
 	}
 
 	@Override
-	public void registerMenuOption(long shopId, long menuId, List<OptionRequest> optionRequests, String userId) {
-		String requestedOwnerId = userMapper.findMerchantByShopId(shopId).getUserId();
-		checkIsAuthorizedUser(requestedOwnerId, userId);
+	public void updateMenuOption(long shopId, OptionTarget optionTarget,AuthInfo authInfo) {
+		// 권한검사
+		Shop shop = shopRepository.findById(shopId)
+			.orElseThrow(() -> new NullPointerException(ErrorUserCode.SHOP_INFO_NOT_EXIST.getMessage()));
+		checkIsAuthorizedUser(shop.getMerchant().getId(), authInfo.getId());
 
-		List<MenuOption> menuOptions = menuOptionMapper.searchMenuOption(menuId);
-		checkIsExistOption(optionRequests, menuOptions);
+		List<MenuOption> menuOptions = menuOptionRepository.findAllByMenuId(optionTarget.getMenuId());
+		checkIsExistUpdateOptionMenu(menuOptions, optionTarget);
 
-		optionRequests.stream()
-			.forEach(optionRequest -> menuOptionMapper.registerOption(optionRequest.toEntity(menuId)));
+		MenuOption menuOption = menuOptionRepository.getById(optionTarget.getMenuOptionId());
+		menuOption.setMenuOptionName(optionTarget.getMenuOptionName());
+		menuOption.setMenuOptionPrice(optionTarget.getMenuOptionPrice());
+
+		menuOptionRepository.save(menuOption);
 	}
 
 	@Override
-	public void updateMenuOption(long shopId, OptionTarget optionTarget, String userId) {
-		String requestedOwnerId = userMapper.findMerchantByShopId(shopId).getUserId();
-		checkIsAuthorizedUser(requestedOwnerId, userId);
-		
-		MenuOption option = menuOptionMapper.searchMenuOptionByOptionId(optionTarget.getMenuOptionId());
-		option.updateOption(optionTarget.getMenuOptionName(), optionTarget.getMenuOptionPrice());
-		menuOptionMapper.updateMenuOption(option);
+	public void deleteMenuOption(long shopId, long menuOptionId,AuthInfo authInfo) {
+		// 권한검사
+		Shop shop = shopRepository.findById(shopId)
+			.orElseThrow(() -> new NullPointerException(ErrorUserCode.SHOP_INFO_NOT_EXIST.getMessage()));
+		checkIsAuthorizedUser(shop.getMerchant().getId(), authInfo.getId());
+
+		menuOptionRepository.deleteById(menuOptionId);
 	}
 
-	@Override
-	public void deleteMenuOption(long shopId, long menuOptionId, String userId) {
-		String requestedOwnerId = userMapper.findMerchantByShopId(shopId).getUserId();
-		checkIsAuthorizedUser(requestedOwnerId, userId);
-		menuOptionMapper.deleteMenuOption(menuOptionId);
-	}
-
-	private void checkIsAuthorizedUser(String requestedId, String userId) {
-		if (!requestedId.equals(userId)) {
+	private void checkIsAuthorizedUser(long requestedId, long userId) {
+		if (requestedId != userId) {
 			throw new UnauthorizedException();
 		}
 	}
 
-	private void checkIsExistOption(List<OptionRequest> optionRequests, List<MenuOption> menuOptions) {
-		for (OptionRequest optionRequest : optionRequests) {
-			Optional<MenuOption> existOption = menuOptions.stream()
-				.filter(m -> m.getMenuOptionName().equals(optionRequest.getMenuOptionName()))
-				.findAny();
-			if (existOption.isPresent()) {
-				throw new IllegalArgumentException(MenuCode.ALEADY_EXIST_MENU.getMessage());
-			}
+	private void checkIsExistOptionMenu(List<MenuOption> menuOptions, OptionRequest optionRequests) {
+		Optional<MenuOption> existMenu = menuOptions.stream()
+			.filter(m -> m.getMenuOptionName().equals(optionRequests.getMenuOptionName()))
+			.findAny();
+
+		if (existMenu.isPresent()) {
+			throw new IllegalArgumentException(MenuCode.ALEADY_EXIST_MENU.getMessage());
+		}
+	}
+
+	private void checkIsExistUpdateOptionMenu(List<MenuOption> menuOptions, OptionTarget optionTarget) {
+		Optional<MenuOption> existMenu = menuOptions.stream()
+			.filter(m -> m.getMenuOptionName().equals(optionTarget.getMenuOptionName()))
+			.findAny();
+
+		if (existMenu.isPresent()) {
+			throw new IllegalArgumentException(MenuCode.ALEADY_EXIST_MENU.getMessage());
 		}
 	}
 }
