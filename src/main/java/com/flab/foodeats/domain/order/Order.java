fe893a933c809d64f8bf3eadc49e3e0b51e8ae9b
Maestro6788 <ensu7928@gmail.com>
2021-11-16 01:48:48 +0900

    refactor: OrderItem 매핑 수정 / Entity

--- src/main/java/com/flab/foodeats/domain/order/Order.java
+++ src/main/java/com/flab/foodeats/domain/order/Order.java
@@ -1,5 +1,6 @@
 package com.flab.foodeats.domain.order;
 
+import java.security.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,7 +31,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-
 @Getter
 @Table(name = "orders")
 @Entity
@@ -38,13 +38,12 @@ import lombok.ToString;
 @ToString
 @AllArgsConstructor
 @Builder
-public class Order {
+public class Order{
 
 	@Id
 	@Column(name = "order_no")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long orderNo;
-
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "consumer_info")
@@ -57,19 +56,24 @@ public class Order {
 	@OneToMany(mappedBy = "order")
 	private List<OrderMenu> orderMenuList = new ArrayList<>();
 
+	@Column(name = "order_time", nullable = false, updatable = false, insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+	public Timestamp orderTime;
+
+
 	@Column(name = "total_price")
 	private int totalPrice;
 
-	@Column(name = "order_status")
-	@ColumnDefault("true")
-	private String orderStatus;
+	@Column(name = "order_status",nullable = false, insertable = false, columnDefinition = "boolean default true")
+	private Boolean orderStatus;
+
+
 
 	// @DynamicInsert vs @PrePersist
 	// https://dotoridev.tistory.com/6
-	@PrePersist
-	public void prePersist() {
-		orderStatus = orderStatus == null ? "true" : orderStatus;
-	}
+	// @PrePersist
+	// public void prePersist() {
+	// 	orderStatus = orderStatus == null ? "true" : orderStatus;
+	// }
 
 	public void setOrderMenuList(List<OrderMenu> orderMenu) {
 		this.orderMenuList = orderMenu;
@@ -79,7 +83,11 @@ public class Order {
 		this.totalPrice = totalPrice;
 	}
 
-	public void setOrderStatus(String orderStatus) {
+	// public void setOrderStatus(Boolean orderStatus) {
+	// 	this.orderStatus = orderStatus;
+	// }
+
+	public void setOrderStatus(Boolean orderStatus) {
 		this.orderStatus = orderStatus;
 	}
 
@@ -91,4 +99,3 @@ public class Order {
 		this.shopInfo = shopInfo;
 	}
 }
-
